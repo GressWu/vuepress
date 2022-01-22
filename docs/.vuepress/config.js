e0cde87fcd8b4df6169cf27a3db146ed0afa0669
gresswu <1162925512@qq.com>
2022-01-22 22:39:38 +0800

    修改配置文件

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,7 +1,13 @@
 module.exports = {
+    title:'武宇威的博客',
+    description:'啦啦啦',
+    head: [
+      ['link', { rel: 'icon', href: '/assetes/img/icon.png' }]
+    ],
     themeConfig: {
       logo: '/assets/img/hero.png',
       sidebar: 'auto',
+      lastUpdated: '更新时间',
       nav: [
         { text: 'Home', link: '/' },
         { text: 'Guide', link: '/guide/' },
