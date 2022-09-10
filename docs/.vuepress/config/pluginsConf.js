37996aa025098b7d87dead9706b4547df07487fc
GressWu <1162925512@qq.com>
2022-09-10 23:09:46 +0800

    删除项目BGM,将BGM上传至OSS对象存储

--- docs/.vuepress/config/pluginsConf.js
+++ docs/.vuepress/config/pluginsConf.js
@@ -12,8 +12,16 @@ module.exports = [
         {
           name: 'А зори здесь тихие-тихие',
           artist: 'Lube',
-          url: '/assets/music/bgm.mp3',
+          //url: '/assets/music/bgm.mp3',  本地歌曲上传
+          url: 'https://md-img-market.oss-cn-beijing.aliyuncs.com/music/%D0%B7%D0%BE%D1%80%D0%B8.mp3',
           cover: 'http://imge.kugou.com/stdmusic/150/20190215/20190215054604117975.jpg'
+        },
+        {
+          name: '手写的从前',
+          artist: '周杰伦',
+          //url: '/assets/music/bgm.mp3',  本地歌曲上传
+          url: 'https://md-img-market.oss-cn-beijing.aliyuncs.com/music/%E6%89%8B%E5%86%99%E7%9A%84%E4%BB%8E%E5%89%8D.mp3',
+          cover: 'https://th.bing.com/th/id/OIP.t6JQrDpw-P1hV8iErO9vpwHaHa?w=169&h=180&c=7&r=0&o=5&pid=1.7'
         }
       ],
       // 是否默认缩小
